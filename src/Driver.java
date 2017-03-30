64e4e76dcb897d89fea95e946c36da0ebba1608a
LTibbetts <Lcubed37@gmail.com>
2017-03-30 12:51:18 -0500

    Updated Driver
    Has some tests in it.

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,30 @@
-import java.util.ArrayList;
 import java.util.Date;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		Calendar c = new Calendar();
 		
+		//Creates a new agenda with an default constructor values
+		Agenda a = new Agenda();
+		
+		//Test events.
+		Event e = new Event("test", new Date(), new Date(), "description test");
+		Event e2 = new Event("test2", new Date(), new Date(), "description test2");
+		
+		//Test notes.
+		Note n = new Note("Test Title", "Test Message"); 
+		Note n2 = new Note("Test Title2", "Test Message2");
+		
+		//Add test events to the agenda's calendar
+		a.getCalendar().addEvent(e);
+		a.getCalendar().addEvent(e2);
+		
+		//Add test notes to the agenda's notepad
+		a.getNotepad().addNote(n);
+		a.getNotepad().addNote(n2);
+		
+		//Print out the agenda data
+		System.out.print(a);
 		
 	}
 
