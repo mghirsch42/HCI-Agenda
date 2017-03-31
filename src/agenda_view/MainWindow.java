0439daa6fe54869cd5c033f9f68a76e6c6ead036
Logan Tibbetts <Lcubed37@gmail.com>
2017-03-31 00:37:04 -0500

    Added JavaFX
    Hey, I added JavaFX to the build path and got the MainWindow.java file
    to have a display that we can work with for developing the GUI. I also
    got rid of the duplicate file and removed the 'default' package.

--- src/agenda_view/MainWindow.java
+++ src/agenda_view/MainWindow.java
@@ -1,24 +1,54 @@
 package agenda_view;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 
-public class MainWindow extends Application {
 
+public class MainWindow extends Application{
+	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		VBox root = new VBox();
-		Scene scene = new Scene(root, 500, 500);
+	public void start(Stage primaryStage) {
+
+		//Border Layout Root
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root,800,600);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				
+		//Menu Bar
+		MenuBar menuBar = new MenuBar();
+		
+		//File
+		Menu file = new Menu("File");
+		MenuItem quit = new MenuItem("Quit"); 
+		quit.setOnAction( (e) -> {
+			primaryStage.close();
+		});
+		
+		//Add items to file
+		file.getItems().add(quit);
+		
+		//Add menus to bar
+		menuBar.getMenus().add(file);
+		
+		//Add Elements to the root.
+		root.setTop(menuBar);
+				
+		//Show the stage. 
+		primaryStage.setTitle("HCI Group Project");
+		primaryStage.setMinWidth(800);
+		primaryStage.setMinHeight(600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-	}
 	
-	//launches the window
-	public void launchWindow() {
-		launch();
+	}
+
+	public static void main(String[] args) {
+		launch(args); 
 	}
 
 }
--- src/agenda_view/WeekPane.java
+++ src/agenda_view/WeekPane.java
@@ -13,9 +13,4 @@ public class WeekPane extends GridPane{
 		
 		
 	}
-	
-	
-	
-	
-	
 }
