2cad7791d7c163175f4de330774cf2ab735e6a2c
LexSCox <eowynacker@Piece-of-Shit.local>
2017-04-30 23:31:26 -0500

    Revert "Revert "Added combo boxes for time""
    This reverts commit aac127964226b90c28d9b69d7a02f68cba572f52.

--- src/agenda_view/BigEventPane.java
+++ src/agenda_view/BigEventPane.java
@@ -39,12 +39,17 @@ public class BigEventPane extends BorderPane{
 	
 	private TextField nameField = new TextField();
 	private TextField descField = new TextField();
+	
 	private DatePicker startPicker = new DatePicker();
-	private TextField startField = new TextField();
+	//private TextField startField = new TextField();
+	private ComboBox<Integer> startHourCombo = new ComboBox<Integer>();
+	private Label startColon = new Label(":");
 	private ComboBox<String> startCombo = new ComboBox<String>();
 	
 	private DatePicker endPicker = new DatePicker();
-	private TextField endField = new TextField();
+	private ComboBox<Integer> endHourCombo = new ComboBox<Integer>();
+	private Label endColon = new Label(":");
+	private ComboBox<Integer> endMinuteCombo = new ComboBox<Integer>();
 	private ComboBox<String> endCombo = new ComboBox<String>();
 	
 	private TextField locField = new TextField();
@@ -78,7 +83,23 @@ public class BigEventPane extends BorderPane{
 				"AM",
 				"PM");		
 				
-		
+		//add the hours to the hour combo boxes
+		for (int i = 1; i <= 12; i++){
+			startHourCombo.getItems().add(i);
+			endHourCombo.getItems().add(i);
+		}
+		//add the minutes to the minute combo boxes
+		for (int i = 0; i <= 60; i++){
+			startMinuteCombo.getItems().add(i);
+			endMinuteCombo.getItems().add(i);
+		}
+		//give all the combo boxes default values
+		startCombo.setValue("PM");
+		endCombo.setValue("PM");
+		startHourCombo.setValue(12);
+		endHourCombo.setValue(1);
+		startMinuteCombo.setValue(0);
+		endMinuteCombo.setValue(0);
 		
 		//Aligning the test fields 
 		nameLbl.setPadding(new Insets(0, 1, 0, 0));
@@ -99,10 +120,12 @@ public class BigEventPane extends BorderPane{
 		catBox.setPadding(new Insets(0, 0, 5, 5));
 		
 		
+		
+		
 		nameBox.getChildren().addAll(nameLbl, nameField);
 		descBox.getChildren().addAll(descLbl, descField);
-		startBox.getChildren().addAll(startLbl, startPicker, startField, startCombo);
-		endBox.getChildren().addAll(endLbl, endPicker, endField, endCombo);
+		startBox.getChildren().addAll(startLbl, startPicker, startHourCombo, startColon, startMinuteCombo, startCombo);
+		endBox.getChildren().addAll(endLbl, endPicker, endHourCombo, endColon, endMinuteCombo, endCombo);
 		locBox.getChildren().addAll(locLbl, locField);
 		colorBox.getChildren().addAll(colorLbl, colorField);
 		catBox.getChildren().addAll(catLbl, catField);
@@ -125,14 +148,13 @@ public class BigEventPane extends BorderPane{
 			// tokens will look like yyyy-mm-dd
 			String[] tokens = date.toString().split("-");
 			
-			String[] time = startField.getText().split(":");
 			
 			GregorianCalendar start = new GregorianCalendar(
 					Integer.parseInt(tokens[0]),	// year
 					Integer.parseInt(tokens[1]) -1, 	// month - not sure why, maybe indexes months from 0?
 					Integer.parseInt(tokens[2]),	// day
-					Integer.parseInt(time[0]),		// hour
-					Integer.parseInt(time[1]));		// minute
+					startHourCombo.getValue(),		// hour
+					startMinuteCombo.getValue());		// minute
 			
 			////////////////////////
 			// Determine end dates
@@ -140,14 +162,13 @@ public class BigEventPane extends BorderPane{
 			
 			date = endPicker.getValue().toString();
 			tokens = date.toString().split("-");
-			time = endField.getText().split(":");
 			
 			GregorianCalendar end = new GregorianCalendar(
 					Integer.parseInt(tokens[0]), // year
 					Integer.parseInt(tokens[1]) -1, 	// month
 					Integer.parseInt(tokens[2]),		// day
-					Integer.parseInt(time[0]),			// hour
-					Integer.parseInt(time[1]));			// minute
+					endHourCombo.getValue(),			// hour
+					endMinuteCombo.getValue());			// minute
 			
 			/////////////////////
 			// Create and add the event
@@ -173,24 +194,54 @@ public class BigEventPane extends BorderPane{
 	
 	// initialize a form with event data
 	private void init(Event event) {
+		
+		//add the hours to the hour combo boxes
+		for (int i = 1; i <= 12; i++){
+			startHourCombo.getItems().add(i);
+			endHourCombo.getItems().add(i);
+		}
+		//add the minutes to the minute combo boxes
+		for (int i = 0; i <= 60; i++){
+			startMinuteCombo.getItems().add(i);
+			endMinuteCombo.getItems().add(i);
+		}
 				
 		nameField.setText(event.getName());
 		descField.setText(event.getDescription());
 		startPicker.setValue(LocalDate.of(event.getStart().get(GregorianCalendar.YEAR), 
 										  event.getStart().get(GregorianCalendar.MONTH)+1, 
 										  event.getStart().get(GregorianCalendar.DATE))); 
-		startField.setText("" + event.getStart().get(GregorianCalendar.HOUR) + ":" + event.getStart().get(GregorianCalendar.MINUTE));
+		
+		//startField.setText("" + event.getStart().get(GregorianCalendar.HOUR) + ":" + event.getStart().get(GregorianCalendar.MINUTE));
+		startHourCombo.setValue(event.getStart().get(GregorianCalendar.HOUR) + 1);
+		startMinuteCombo.setValue(event.getStart().get(GregorianCalendar.MINUTE));
 		startCombo.getItems().addAll(
 				"AM",
 				"PM");		
+		//check if start time is am or pm
+		if (event.getStart().get(GregorianCalendar.AM_PM) == GregorianCalendar.AM){
+			startCombo.setValue("AM");
+		} else {
+			startCombo.setValue("PM");
+		}
+		
 		endPicker.setValue(LocalDate.of(event.getEnd().get(GregorianCalendar.YEAR), 
 										event.getEnd().get(GregorianCalendar.MONTH)+1, 
 										event.getEnd().get(GregorianCalendar.DATE))); 
-		endField.setText("" + event.getEnd().get(GregorianCalendar.HOUR) + ":" + event.getEnd().get(GregorianCalendar.MINUTE));
+		
+		//endField.setText("" + event.getEnd().get(GregorianCalendar.HOUR) + ":" + event.getEnd().get(GregorianCalendar.MINUTE));
+		endHourCombo.setValue(event.getEnd().get(GregorianCalendar.HOUR) + 1);
+		endMinuteCombo.setValue(event.getEnd().get(GregorianCalendar.MINUTE));
 		endCombo.getItems().addAll(
 				"AM",
 				"PM");		
-
+		//check if end time is am or pm
+		if (event.getEnd().get(GregorianCalendar.AM_PM) == GregorianCalendar.AM){
+			endCombo.setValue("AM");
+		} else {
+			endCombo.setValue("PM");
+		}
+		
 		locField.setText(event.getLocation());
 		colorField.setText(event.getColor());
 		catField.setText(event.getCategory());
@@ -216,8 +267,8 @@ public class BigEventPane extends BorderPane{
 		
 		nameBox.getChildren().addAll(nameLbl, nameField);
 		descBox.getChildren().addAll(descLbl, descField);
-		startBox.getChildren().addAll(startLbl, startPicker, startField, startCombo);
-		endBox.getChildren().addAll(endLbl, endPicker, endField, endCombo);
+		startBox.getChildren().addAll(startLbl, startPicker, startHourCombo, startColon, startMinuteCombo, startCombo);
+		endBox.getChildren().addAll(endLbl, endPicker, endHourCombo, endColon, endMinuteCombo, endCombo);
 		locBox.getChildren().addAll(locLbl, locField);
 		colorBox.getChildren().addAll(colorLbl, colorField);
 		catBox.getChildren().addAll(catLbl, catField);
@@ -230,27 +281,24 @@ public class BigEventPane extends BorderPane{
 			
 			// tokens will look like yyyy-mm-dd
 			String[] tokens = date.toString().split("-");
-			
-			String[] time = startField.getText().split(":");
-			
+		
 			GregorianCalendar start = new GregorianCalendar(
 					Integer.parseInt(tokens[0]),	// year - date takes years since 1900
 					Integer.parseInt(tokens[1]) -1, 	// month - not sure why, maybe indexes months from 0?
 					Integer.parseInt(tokens[2]),	// day
-					Integer.parseInt(time[0]),		// hour
-					Integer.parseInt(time[1]));		// minute
+					startHourCombo.getValue(),		// hour
+					startMinuteCombo.getValue());		// minute
 			event.setStart(start);
 			
 			date = endPicker.getValue().toString();
 			tokens = date.toString().split("-");
-			time = endField.getText().split(":");
 			
 			GregorianCalendar end = new GregorianCalendar(
 					Integer.parseInt(tokens[0]), // year
 					Integer.parseInt(tokens[1]) -1, 	// month
 					Integer.parseInt(tokens[2]),		// day
-					Integer.parseInt(time[0]),			// hour
-					Integer.parseInt(time[1]));			// minute
+					endHourCombo.getValue(),			// hour
+					endMinuteCombo.getValue());			// minute
 			event.setEnd(end);
 			event.setColor(colorField.getText());
 			event.setCategory(catField.getText());
